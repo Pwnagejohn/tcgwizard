464552a379f6051468f61c204db490c3d49aed01
Pwnagejohn <joe_black89@hotmail.it>
2023-10-08 11:39:13 +0200

    First commit

---
+++ src/main/java/side/project/tcgwizard/TcgwizardApplication.java
@@ -0,0 +1,13 @@
+package side.project.tcgwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgwizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgwizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/side/project/tcgwizard/TcgwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package side.project.tcgwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgwizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
